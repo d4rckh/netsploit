0af6a4a0f7d69db302e6179f8fc21528700a2c6a
m4d-d0x <m4dd0x.xxx@gmail.com>
2020-03-27 20:35:04 +0000

    arpscan

--- attacks/arpscan.py
+++ attacks/arpscan.py
@@ -1,2 +1,22 @@
-def start(range):
-    print("Range: " + range)
+from scapy.all import ARP, Ether, srp
+from time import sleep
+
+def start(range, wait):
+    arp = ARP(pdst=range)
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+    packet = ether/arp
+    result = srp(packet, timeout=3, verbose=0)[0]
+    clients = []
+
+    for sent, received in result:
+        res = {'ip': received.psrc, 'mac': received.hwsrc}
+        if res not in clients:
+            clients.append(res)
+    sleep(wait)
+    print("Wait time for arp replies: " + str(wait) + "s")
+    print("Available devices:")
+    print("IP\t\tMAC")
+    print("-"*33)
+    for client in clients:
+        print(client["ip"] + "\t" + client["mac"])
+
--- main.py
+++ main.py
@@ -4,14 +4,18 @@ from attacks.arpscan import start as arpscan
 parser = argparse.ArgumentParser()
 parser.add_argument('--attack', required=True)
 parser.add_argument('--range', required=False)
+parser.add_argument('--wait', required=False, type=int)
 args = parser.parse_args()
 
 attack = args.attack
 
+wait = args.wait
 range = args.range
 
 if attack == "arpscan":
     if range == None:
-        print("Range not specified: --range")
+        print("Range not specified: --range (it's probably 192.168.0.1/24)")
         quit()
-    arpscan(range)
+    if wait == None:
+        wait = 2
+    arpscan(range,wait)
